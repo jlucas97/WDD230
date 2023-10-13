d492a4a6c09bc02fe7b61fe66609fc91f772e5ab
jlucas97 <jeanlucascastillo@hotmail.com>
2023-10-13 16:13:04 -0600

    Learning activities from Week 4 done

---
+++ week05/scripts/weather.js
@@ -0,0 +1,32 @@
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=3c0438352314bba7fae97ccf68bf51b5';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = data.main.temp + " F degrees";
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', `Weather ${desc}`);
+    captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
